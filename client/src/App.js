857164082dfcb319e829e30eabba2c8c630b595b
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-21 14:30:34 -0300

    Adiciona container de perguntas no App e divide as seções para perguntas e candidatos

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 
+import "./App.css";
+
 // Layout imports
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import PerguntasContainer from "./components/perguntas/PerguntasContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <PerguntasContainer />
+            </div>
+            <div className="col-6">Candidatos</div>
+          </div>
+        </div>
         <Footer />
       </div>
     );
