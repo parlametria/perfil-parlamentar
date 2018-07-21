9a8477fd2799ad6da504645e244604d1c0f28072
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-21 15:42:06 -0300

    Move json para /data

--- client/src/components/perguntas/PerguntasContainer.js
+++ client/src/components/perguntas/PerguntasContainer.js
@@ -1,27 +1,9 @@
 import React, { Component } from "react";
 import Pergunta from "./Pergunta";
+import jsonPerguntas from "../../data/perguntas.json";
 
 class VotacoesContainer extends Component {
   render() {
-    const jsonPerguntas = [
-      {
-        tema: "Segurança",
-        perguntas: [{ pergunta: "Você deseja a paz mundial?", autor: "OC" }]
-      },
-      {
-        tema: "Educação",
-        perguntas: [
-          {
-            pergunta: "Você deseja melhorar a educação no Brasil?",
-            autor: "OC"
-          },
-          {
-            pergunta: "Você deseja melhorar a educação no Brasil?",
-            autor: "OC"
-          }
-        ]
-      }
-    ];
     const perguntas = jsonPerguntas.map((elem, i) => {
       let perguntasDoTema = elem.perguntas.map((elem, i) => (
         <Pergunta key={i} pergunta={elem.pergunta} autor={elem.autor} />
