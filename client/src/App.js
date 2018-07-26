1abf822638269f3a426e0dbbc04e9e252793aca8
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-26 17:06:26 -0300

    Adiciona imports do firebase, inicializa redux no App component e checa token

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 
 import "./App.css";
 
+// Router Stuff
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Redux imports
+import { Provider } from "react-redux";
+import store from "./store";
+
 // Layout imports
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -10,23 +17,36 @@ import Footer from "./components/layout/Footer";
 import PerguntasContainer from "./components/perguntas/PerguntasContainer";
 import CandidatosContainer from "./components/candidatos/CandidatosContainer";
 
+// Login and logout actions
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
+import { firebaseImpl } from "./services/firebaseService";
+
+// Check token
+if (localStorage.accessToken) {
+  const user = JSON.parse(localStorage.user);
+  store.dispatch(setCurrentUser(user));
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 col-sm-12">
-              <PerguntasContainer />
-            </div>
-            <div className="col-md-6 col-sm-12">
-              <CandidatosContainer />
+      <Provider store={store}>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 col-sm-12">
+                <PerguntasContainer />
+              </div>
+              <div className="col-md-6 col-sm-12">
+                <CandidatosContainer />
+              </div>
             </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Provider>
     );
   }
 }
