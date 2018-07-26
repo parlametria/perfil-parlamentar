e84bc9d6a3f2beb572c833d04047d783dce085f7
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-26 17:02:23 -0300

    Configura as chaves de acesso ao firebase
    Adiciona a chave do banco de dev ao .gitignore

---
+++ client/src/config/keys_prod.js
@@ -0,0 +1,10 @@
+// const keys_prod = {
+//   apiKey: process.ENV.apiKey,
+//   authDomain: process.ENV.authDomain,
+//   databaseURL: process.ENV.databaseURL,
+//   projectId: process.ENV.projectId,
+//   storageBucket: process.ENV.storageBucket,
+//   messagingSenderId: process.ENV.messagingSenderId
+// };
+
+// export default keys_prod;
