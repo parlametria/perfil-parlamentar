191a06c00598abedaa6c4bb3a5fb2f89f8ffbba9
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-26 17:09:34 -0300

    Cria arquivo responsável pela inicialização do firebase

---
+++ client/src/services/firebaseService.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+import keys from "../config/keys";
+
+const config = {
+  apiKey: keys.apiKey,
+  authDomain: keys.authDomain,
+  databaseURL: keys.databaseURL,
+  projectId: keys.projectId,
+  storageBucket: keys.storageBucket,
+  messagingSenderId: keys.messagingSenderId
+};
+
+export const firebaseImpl = firebase.initializeApp(config);
+export const firebaseDatabase = firebase.database();
+export const provider = new firebase.auth.GoogleAuthProvider();
