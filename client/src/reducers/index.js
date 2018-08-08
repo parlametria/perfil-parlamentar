7bfec7182c263dfb852edac0db3df85f056a55a4
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-08 18:17:47 -0300

    Adiciona userReducer e candidatesReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import userReducer from "./userReducer";
+import candidatesReducer from "./candidatesReducer";
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  userReducer: userReducer,
+  candidatesReducer: candidatesReducer
 });
