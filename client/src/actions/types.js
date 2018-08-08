539dae29633247a8b79a4fba52e5cbcc2cc4c7ba
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-08 18:06:12 -0300

    Adiciona ações de SET_USER_SCORE e SET_CANDIDATE_SCORE

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,6 @@
 // Users types
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const SET_USER_SCORE = "SET_USER_SCORE";
+
+// Candidate types
+export const SET_CANDIDATE_SCORE = "SET_CANDIDATE_SCORE";
