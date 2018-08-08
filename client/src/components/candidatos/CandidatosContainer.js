169ed80d9714df09993c1c3ce8e86b0ced40836b
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-08 18:16:04 -0300

    Adiciona funcionalidades de cálculo de score
    Cria função para criação dos candidatos
    Cria chamadas às actions
    Cria conexão com os reducers

--- client/src/components/candidatos/CandidatosContainer.js
+++ client/src/components/candidatos/CandidatosContainer.js
@@ -1,19 +1,68 @@
 import React, { Component } from "react";
 import Candidato from "./Candidato";
 
+import { connect } from "react-redux";
+
+import { withRouter } from "react-router-dom";
+
+import { calculateScore } from "../../actions/candidatesActions";
+
+import PropTypes from "prop-types";
+
 class CandidatosContainer extends Component {
+  constructor(props) {
+    super(props);
+    props.candidateVotings = {};
+
+    this.state = {
+      candidatesVotings: {},
+      candidatesScore: {},
+      candidatos: {}
+    };
+  }
+
   render() {
-    return (
-      <div className="container candidatos-container">
+    const candidatos = Object.keys(this.state.candidatos).map(elem => {
+      const candidato = this.state.candidatos[elem];
+
+      return (
         <Candidato
-          key={1}
-          nome="Luiza Erundina"
-          siglaPartido="PSOL"
-          estado="SP"
+          key={candidato.id}
+          nome={candidato.nome}
+          siglaPartido={candidato.siglaPartido}
+          estado={candidato.estado}
+          score={this.state.candidatesScore[candidato.id]}
+          votacoes={this.props.candidatesVotings[candidato.id]}
         />
-      </div>
-    );
+      );
+    });
+
+    return <div className="container candidatos-container">{candidatos}</div>;
+  }
+
+  componentDidMount() {
+    // Recuperar do BD as votações uma única vez.
+    // firebase.database.get...
+
+    this.props.calculateScore(this.props.candidatesVotings);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.candidates.candidatesScore) {
+      console.log(nextProps.candidates.candidatesScore);
+      this.setState({ candidatesScore: nextProps.candidates.candidatesScore });
+    }
   }
 }
 
-export default CandidatosContainer;
+CandidatosContainer.propTypes = {
+  calculateScore: PropTypes.func.isRequired
+};
+const mapStateToProps = state => ({
+  candidates: state.candidatesReducer
+});
+
+export default connect(
+  mapStateToProps,
+  { calculateScore }
+)(CandidatosContainer);
