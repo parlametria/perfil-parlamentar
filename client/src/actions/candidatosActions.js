1f430aa342da2e3f1cefd8dc63d467cfc533a45f
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-09 11:51:59 -0300

    Ajusta detalhes em candidatosActions
    Ajusta retorno para dicionário
    Adiciona nova função para retornar candidatos ordenados pelo score

--- client/src/actions/candidatosActions.js
+++ client/src/actions/candidatosActions.js
@@ -1,7 +1,9 @@
 import { SET_SCORE_CANDIDATO } from "./types";
 
+// Recebe um dicionário das respostas dos candidatos no formato {id_cand: [array_resp]} e retorna um dicionário no formato {id_cand: score}
 export const calculaScore = respostasCandidatos => (dispatch, getState) => {
   const { arrayRespostasUsuario } = getState().usuarioReducer;
+
   const numRespostasUsuario = arrayRespostasUsuario.filter(value => value !== 0)
     .length;
 
@@ -17,16 +19,34 @@ export const calculaScore = respostasCandidatos => (dispatch, getState) => {
     return respostasIguais / numRespostasUsuario;
   }
 
-  const scoreCandidatos = Object.keys(respostasCandidatos).map(elem => {
+  let scoreCandidatos = {};
+  Object.keys(respostasCandidatos).map(elem => {
     let score = comparaRespostas(respostasCandidatos[elem]);
-
-    return { id_cand: elem, score };
+    scoreCandidatos[elem] = score;
   });
-
-  console.log(scoreCandidatos);
 
   dispatch({
     type: SET_SCORE_CANDIDATO,
     scoreCandidatos
   });
+};
+
+// Pega o top n candidatos baseado na compatibilidade entre as respostas ordenado pelo score. Recebe um dicionário das respostas dos candidatos e retorna um array com os ids dos candidatos.
+export const getTopNCandidatos = (respostasCandidatos, n) => (
+  dispatch,
+  getState
+) => {
+  const { scoreCandidatos } = getState().candidatosReducer;
+  let candidatos = Object.keys(scoreCandidatos).map(key => [
+    key,
+    scoreCandidatos[key]
+  ]);
+
+  candidatos.sort((a, b) => {
+    if (a[1] > b[1]) return -1;
+    else if (a[1] === b[1]) return 0;
+    else return 1;
+  });
+
+  return candidatos.slice(0, n);
 };
