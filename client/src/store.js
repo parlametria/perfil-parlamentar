2d1bbb2259a7e4ccf6ea5962736fe080890b3237
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-10 12:49:58 -0300

    Resolve bug da extensão do ReduxDevTools

--- client/src/store.js
+++ client/src/store.js
@@ -36,7 +36,9 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+      compose
   )
 );
 
