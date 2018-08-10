2e09e47547fc98c93ef3e134b4b59c1a7bc294a8
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-10 07:57:11 -0300

    Adiciona registro de resposta, id na pergunta e chamada às ações de cálculo de score

--- client/src/components/perguntas/PerguntasContainer.js
+++ client/src/components/perguntas/PerguntasContainer.js
@@ -1,12 +1,45 @@
 import React, { Component } from "react";
 import Pergunta from "./Pergunta";
+
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { salvaScoreUsuario } from "../../actions/usuarioActions";
+import { calculaScore } from "../../actions/candidatosActions";
+
 import jsonPerguntas from "../../data/perguntas.json";
 
 class PerguntasContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { respostasUsuario: {}, arrayRespostasUsuario: [] };
+  }
+
+  registraResposta(novaResposta) {
+    let { respostasUsuario } = this.state;
+    let { arrayRespostasUsuario } = this.state;
+    respostasUsuario[novaResposta.id] = novaResposta.resposta;
+    arrayRespostasUsuario[novaResposta.id] = novaResposta.resposta;
+    this.props.salvaScoreUsuario(respostasUsuario, arrayRespostasUsuario);
+    this.props.calculaScore();
+  }
+
+  componentDidMount() {
+    const { respostasUsuario } = this.props.usuario;
+    const { arrayRespostasUsuario } = this.props.usuario;
+    this.setState({ respostasUsuario, arrayRespostasUsuario });
+  }
+
   render() {
     const perguntas = jsonPerguntas.map((elem, i) => {
       let perguntasDoTema = elem.perguntas.map((elem, i) => (
-        <Pergunta key={i} pergunta={elem.pergunta} autor={elem.autor} />
+        <Pergunta
+          key={i}
+          id={i}
+          pergunta={elem.pergunta}
+          autor={elem.autor}
+          onVota={novaResposta => this.registraResposta(novaResposta)}
+        />
       ));
 
       return (
@@ -22,4 +55,15 @@ class PerguntasContainer extends Component {
   }
 }
 
-export default PerguntasContainer;
+PerguntasContainer.propTypes = {
+  salvaScoreUsuario: PropTypes.func.isRequired
+};
+const mapStateToProps = state => ({
+  usuario: state.usuarioReducer,
+  candidatos: state.candidatosReducer
+});
+
+export default connect(
+  mapStateToProps,
+  { salvaScoreUsuario, calculaScore }
+)(PerguntasContainer);
