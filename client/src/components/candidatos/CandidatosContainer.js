0e063055ab1fa61fe87de4d93cd7fc3f7a6c7d7e
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-10 12:36:15 -0300

    Adiciona flip-move e renderização condicional

--- client/src/components/candidatos/CandidatosContainer.js
+++ client/src/components/candidatos/CandidatosContainer.js
@@ -3,14 +3,14 @@ import Candidato from "./Candidato";
 
 import { connect } from "react-redux";
 
+import FlipMove from "react-flip-move";
+
 import {
   calculaScore,
   getTopNCandidatos
 } from "../../actions/candidatosActions";
 
 import PropTypes from "prop-types";
-
-import dadosCandidatos from "../../data/data.json";
 
 const NUM_CANDIDATOS = 10;
 
@@ -25,22 +25,34 @@ class CandidatosContainer extends Component {
   }
 
   render() {
+    console.log(this.props.candidatos);
     const candidatos = this.state.candidatosAExibir.map(elem => {
-      const candidato = dadosCandidatos[elem[0]];
+      const candidato = this.props.candidatos.dadosCandidatos[elem[0]];
+
+      console.log(this.props.candidatos);
+      console.log(this.props.candidatos.dbStatus["carregando"]);
 
       return (
         <Candidato
-          key={candidato.idCandidato}
-          nome={"Candidata " + candidato.idCandidato}
+          key={candidato.id}
+          nome={"Candidata " + candidato.id}
           siglaPartido={"candidato.siglaPartido"}
           estado={"candidato.estado"}
-          score={this.state.scoreCandidatos[candidato.idCandidato]}
+          score={this.state.scoreCandidatos[candidato.id]}
           respostas={candidato.respostas}
         />
       );
     });
 
-    return <div className="container candidatos-container">{candidatos}</div>;
+    return (
+      <div className="container candidatos-container">
+        {this.props.candidatos.dbStatus["carregando"] ? (
+          <div>carregando</div>
+        ) : (
+          <FlipMove>{candidatos}</FlipMove>
+        )}
+      </div>
+    );
   }
 
   componentWillReceiveProps(nextProps) {
