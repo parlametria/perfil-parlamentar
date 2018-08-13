e493a0b8bfd64c423de78efcb2845ce112bbf804
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-13 20:53:23 -0300

    Inicializa reducer de perguntas e faz chamada no componente de perguntas

---
+++ client/src/actions/perguntasActions.js
@@ -0,0 +1,34 @@
+import { PERGUNTAS_CARREGANDO, SET_DADOS_PERGUNTAS } from "./types";
+import { firebaseDatabase } from "../services/firebaseService";
+
+export const getDadosPerguntas = () => dispatch => {
+  dispatch(setPerguntasCarregando());
+
+  let dadosPerguntas = {};
+
+  firebaseDatabase
+    .ref("/perguntas")
+    .once("value")
+    .then(snapshot => {
+      const perguntas = snapshot.val();
+      perguntas.map(elem => {
+        let arrPerguntas = dadosPerguntas[elem.tema];
+
+        if (arrPerguntas) {
+          arrPerguntas.push({ key: elem.key, pergunta: elem.pergunta });
+          dadosPerguntas[elem.tema] = arrPerguntas;
+        } else {
+          let arrayInicial = [];
+          arrayInicial.push({ key: elem.key, pergunta: elem.pergunta });
+          dadosPerguntas[elem.tema] = arrayInicial;
+        }
+      });
+
+      dispatch({ type: SET_DADOS_PERGUNTAS, dadosPerguntas });
+    })
+    .catch(err => console.log(err));
+};
+
+export const setPerguntasCarregando = () => {
+  return { type: PERGUNTAS_CARREGANDO };
+};
