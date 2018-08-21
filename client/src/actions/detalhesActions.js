ba646b5e6080bafe06d6c4c7ac4318181c15b211
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-21 12:04:27 -0300

    Adiciona nome de classe na div

--- client/src/actions/detalhesActions.js
+++ client/src/actions/detalhesActions.js
@@ -1,6 +1,5 @@
-/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
-import React, {Component} from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import React, { Component } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 class MostraDetalhes extends Component {
   constructor(props) {
@@ -18,29 +17,34 @@ class MostraDetalhes extends Component {
       modal: !this.state.modal,
       tooltipOpen: !this.state.tooltipOpen
     });
-  }   
+  }
 
   render() {
     const dados = this.props.Dados;
     return (
-
-      <div>
+      <div className="detalhe-deputado">
         <img
-              src="http://www.luizaerundina.com.br/images/site/luiza.png"
-              alt="..."
-              width="100px"
-              height="100px"
-              className="img-thumbnail avatar rounded-circle"
-              onClick={this.toggle}
-            />
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          src="http://www.luizaerundina.com.br/images/site/luiza.png"
+          alt="..."
+          width="100px"
+          height="100px"
+          className="img-thumbnail avatar rounded-circle"
+          onClick={this.toggle}
+        />
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
           <ModalHeader toggle={this.toggle}> {dados.nome} </ModalHeader>
           <ModalBody>
             Partido: {this.props.Dados.siglaPartido} <br />
             UF: {this.props.Dados.estado}
           </ModalBody>
           <ModalFooter>
-            <Button color="secondary" onClick={this.toggle}>Fechar</Button>
+            <Button color="secondary" onClick={this.toggle}>
+              Fechar
+            </Button>
           </ModalFooter>
         </Modal>
       </div>
