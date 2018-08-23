f959802d738f4d2df099023cbb86f8799a09e9b4
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-23 11:56:15 -0300

    Muda estrutura do estado do componente
    - Remove o estado local e delega tudo para o reducer
    - Importa ações para passar as perguntas
    - Adiciona seleção de pergunta por tema

--- client/src/components/perguntas/PerguntasContainer.js
+++ client/src/components/perguntas/PerguntasContainer.js
@@ -9,7 +9,9 @@ import { calculaScore } from "../../actions/candidatosActions";
 import {
   getDadosPerguntas,
   voltaPergunta,
-  passaPergunta
+  passaPergunta,
+  escolhePergunta,
+  escolheTema
 } from "../../actions/perguntasActions";
 
 import Spinner from "../common/Spinner";
@@ -18,20 +20,14 @@ import isEmpty from "../../validation/is-empty";
 class PerguntasContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      respostasUsuario: {},
-      arrayRespostasUsuario: [],
-      indexPerguntaAtual: 0,
-      categoriaPergunta: "Meio Ambiente"
-    };
+
     this.passaPergunta = this.passaPergunta.bind(this);
     this.voltaPergunta = this.voltaPergunta.bind(this);
     this.selecionaTema = this.selecionaTema.bind(this);
   }
 
   registraResposta(novaResposta) {
-    let { respostasUsuario } = this.state;
-    let { arrayRespostasUsuario } = this.state;
+    const { respostasUsuario, arrayRespostasUsuario } = this.props.usuario;
     respostasUsuario[novaResposta.id] = novaResposta.resposta;
     arrayRespostasUsuario[novaResposta.id] = novaResposta.resposta;
     this.props.salvaScoreUsuario(respostasUsuario, arrayRespostasUsuario);
@@ -49,51 +45,24 @@ class PerguntasContainer extends Component {
 
   selecionaTema(e) {
     e.preventDefault();
-    this.setState({
-      categoriaPergunta: e.target.id
+    const { dadosPerguntas } = this.props.perguntas;
+    const perguntasFiltradas = dadosPerguntas.filter(pergunta => {
+      return pergunta.tema === e.target.id;
     });
+    this.props.escolheTema(e.target.id);
+    this.props.escolhePergunta(perguntasFiltradas[0].id);
   }
 
   componentDidMount() {
-    const { respostasUsuario, arrayRespostasUsuario } = this.props.usuario;
-    const { perguntaAtual } = this.props.perguntas;
     this.props.getDadosPerguntas();
-    this.setState({
-      respostasUsuario,
-      arrayRespostasUsuario,
-      indexPerguntaAtual: perguntaAtual
-    });
   }
 
   render() {
     const {
       dadosPerguntas,
-      perguntaAtual,
+      indexPergunta,
       isCarregando
     } = this.props.perguntas;
-
-    const perguntas = Object.keys(dadosPerguntas).map(tema => {
-      let perguntasDoTema = dadosPerguntas[tema].map(pergunta => {
-        let { arrayRespostasUsuario } = this.props.usuario;
-        return (
-          <Pergunta
-            key={pergunta.key}
-            id={pergunta.key}
-            pergunta={pergunta.pergunta}
-            voto={arrayRespostasUsuario[pergunta.key]}
-            onVota={novaResposta => this.registraResposta(novaResposta)}
-          />
-        );
-      });
-
-      return (
-        <div key={tema} className="container">
-          <h4>{tema}</h4>
-          <div>{perguntasDoTema}</div>
-          <div className="dropdown-divider" />
-        </div>
-      );
-    });
 
     const botoesNavegacao = (
       <div>
@@ -107,29 +76,38 @@ class PerguntasContainer extends Component {
     );
 
     let pergunta;
-    let temas;
+    let temas = [];
 
     if (!isEmpty(dadosPerguntas)) {
-      const dadosPergunta = dadosPerguntas[this.state.categoriaPergunta];
+      const dadosPergunta = dadosPerguntas[indexPergunta];
+
       const { arrayRespostasUsuario } = this.props.usuario;
 
-      temas = Object.keys(dadosPerguntas).map(elem => (
-        <div
-          className="btn btn-dark col"
-          key={elem}
-          id={elem}
-          onClick={this.selecionaTema}
-        >
-          {elem}
-        </div>
-      ));
+      // Constrói os eixos (isso idealmente deve vir de um bd, algo assim)
+      let nomeTemas = new Set();
+      dadosPerguntas.map(pergunta => {
+        nomeTemas.add(pergunta.tema);
+      });
+
+      Array.from(nomeTemas).map((tema, i) => {
+        temas.push(
+          <div
+            id={tema}
+            onClick={this.selecionaTema}
+            key={i + ". " + tema}
+            className="btn btn-dark col"
+          >
+            {tema}
+          </div>
+        );
+      });
 
       pergunta = (
         <Pergunta
-          key={dadosPergunta[perguntaAtual].key}
-          id={dadosPergunta[perguntaAtual].key}
-          pergunta={dadosPergunta[perguntaAtual].pergunta}
-          voto={arrayRespostasUsuario[dadosPergunta[perguntaAtual].key]}
+          key={dadosPergunta.id}
+          id={dadosPergunta.id}
+          pergunta={dadosPergunta.texto}
+          voto={arrayRespostasUsuario[dadosPergunta.id]}
           onVota={novaResposta => this.registraResposta(novaResposta)}
         />
       );
@@ -143,7 +121,6 @@ class PerguntasContainer extends Component {
           <div>
             <div className="row">{temas}</div>
             <div className="row">{pergunta}</div>
-            <div className="row">{pergunta.key}</div>
             <div className="row">{botoesNavegacao}</div>
           </div>
         )}
@@ -155,7 +132,11 @@ class PerguntasContainer extends Component {
 PerguntasContainer.propTypes = {
   salvaScoreUsuario: PropTypes.func.isRequired,
   calculaScore: PropTypes.func.isRequired,
-  getDadosPerguntas: PropTypes.func.isRequired
+  getDadosPerguntas: PropTypes.func.isRequired,
+  passaPergunta: PropTypes.func.isRequired,
+  voltaPergunta: PropTypes.func.isRequired,
+  escolhePergunta: PropTypes.func.isRequired,
+  escolheTema: PropTypes.func.isRequired
 };
 const mapStateToProps = state => ({
   usuario: state.usuarioReducer,
@@ -170,6 +151,8 @@ export default connect(
     calculaScore,
     getDadosPerguntas,
     passaPergunta,
-    voltaPergunta
+    voltaPergunta,
+    escolhePergunta,
+    escolheTema
   }
 )(PerguntasContainer);
