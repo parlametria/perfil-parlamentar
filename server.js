4a9c7f9d853ca4be621f4adee358a90dc9928aba
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-29 20:28:13 -0300

    Resolve problema de rotas - api pode ser acessada pelos links

--- server.js
+++ server.js
@@ -29,15 +29,12 @@ app.use("/api/perguntas", perguntas);
 app.use("/api/candidatos", candidatos);
 app.use("/api/respostas", respostas);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// Set static folder
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 
