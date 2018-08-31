e9ab80e5f4ce0f1fff98d0a985b3ebefe78eadfb
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-08-31 18:14:38 -0300

    Tenta novamente usar compressão

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const compression = require('compression')
 
 const perguntas = require("./routes/api/perguntas");
 const candidatos = require("./routes/api/candidatos");
 const respostas = require("./routes/api/respostas");
 
 const app = express();
+
+app.use(compression())
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
