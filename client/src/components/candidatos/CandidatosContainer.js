7a3478888bc801812fbb19a9b03e648254e3a8e2
Jefferson Neves <jefferson.rpn@gmail.com>
2018-08-31 19:35:32 -0300

    Renderiza apresentação condicionalmente

--- client/src/components/candidatos/CandidatosContainer.js
+++ client/src/components/candidatos/CandidatosContainer.js
@@ -17,6 +17,8 @@ import {
 import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
 
+import Apresentacao from "./apresentacao";
+
 const NUM_CANDIDATOS = 10;
 
 class CandidatosContainer extends Component {
@@ -109,24 +111,7 @@ class CandidatosContainer extends Component {
     return (
       <div className="container">
         <header className="panel-header">
-          <h2 className="panel-title text-center">Vamos começar?</h2>
-          <div>
-            <p>
-              <strong>Vote</strong> nos temas propostos e veja aqui os
-              candidatos que mais se alinham com você.
-            </p>
-            <p>
-              <strong>Filtre</strong> o resultado segundo seus critérios.
-            </p>
-            <p>
-              <strong>Informe-se</strong> sobre os 5 temas. Quanto mais
-              perguntas você responder, mas preciso é o resultado.
-            </p>
-
-            <div className="text-center">
-              <button className="btn btn-primary">Mostre-me todos</button>
-            </div>
-          </div>
+          {false ? <Apresentacao /> : null }
           <div className="input-group mb-3">
             <div className="input-group-prepend">
               <span
@@ -198,9 +183,9 @@ class CandidatosContainer extends Component {
   }
 
   componentDidMount() {
-    if (isEmpty(this.props.candidatos.dadosCandidatos))
-      this.props.getDadosCandidatos();
-    else {
+    if (isEmpty(this.props.candidatos.dadosCandidatos)) {
+      // this.props.getDadosCandidatos();
+    } else {
       this.setState({
         scoreCandidatos: this.props.candidatos.scoreCandidatos,
         candidatosRanking: this.props.getTopNCandidatos(NUM_CANDIDATOS)
---
+++ client/src/components/candidatos/apresentacao.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h2 className="panel-title text-center">Vamos começar?</h2>
+    <div>
+      <p>
+        <strong>Vote</strong> nos temas propostos e veja aqui os
+        candidatos que mais se alinham com você.
+      </p>
+      <p>
+        <strong>Filtre</strong> o resultado segundo seus critérios.
+      </p>
+      <p>
+        <strong>Informe-se</strong> sobre os 5 temas. Quanto mais
+        perguntas você responder, mas preciso é o resultado.
+      </p>
+
+      <div className="text-center">
+        <button className="btn btn-primary">Mostre-me todos</button>
+      </div>
+    </div>
+  </div>
+)
