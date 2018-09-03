ab3c4bec44e3cf9830c54913327dfe5c4d543ae0
Jefferson Neves <jefferson.rpn@gmail.com>
2018-09-03 19:39:10 -0300

    Cria página de 'sobre'

---
+++ client/src/components/sobre/Sobre.js
@@ -0,0 +1,183 @@
+import React, { Component } from "react";
+
+class Sobre extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <section className="intro">
+            <h2 className="intro-title text-center">
+              Voz ativa quer auxiliar a sua{" "}
+              <strong className="strong">decisão de voto</strong> a partir de
+              temas de fundamental importância para o Brasil.
+            </h2>
+          </section>
+          <section className="section">
+            <div className="row">
+              <div className="col-md-6 col-lg-5">
+                <h4 className="footer-title">Quem somos?</h4>
+                <p>
+                  A plataforma Voz Ativa foi construída a partir de questões
+                  formuladas pela Rede de Advocacy, um coletivo de 30
+                  Organizações da Sociedade Civil que atuam nos temas de
+                  direitos humanos, integridade e transparência,
+                  meio-ambiente/clima e negócios de impacto para uma nova
+                  economia. O nosso foco principal é auxiliar os eleitores na
+                  sua decisão de voto a partir de temas de fundamental
+                  importância para o Brasil.
+                </p>
+              </div>
+              <div className="col-md-6 col-lg-4">
+                <h4 className="footer-title">O que é uma Rede Advocacy?</h4>
+                <p>
+                  Somos uma rede de 30 organizações da sociedade civil que atuam
+                  para uma nova economia.
+                </p>
+              </div>
+              <div className="col-md col-lg-3">
+                <p>
+                  <strong className="strong">
+                    Voz ativa pode te ajudar a monitorar a coerência do seu
+                    candidato
+                  </strong>
+                  . Após a eleição, o eleitor poderá acessar a nossa base para
+                  saber se o candidato eleito está agindo de acordo com suas
+                  respostas ou não.
+                </p>
+              </div>
+            </div>
+          </section>
+          <section className="section">
+            <h4 className="footer-title">Como funciona?</h4>
+            <div className="row">
+              <div className="col-md-6">
+                <img
+                  src={require("../../data/img/howto01.jpg")}
+                  alt="Como funciona"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-6">
+                <img
+                  src={require("../../data/img/howto02.jpg")}
+                  alt="Como funciona"
+                  width="100%"
+                />
+              </div>
+            </div>
+          </section>
+        </div>
+        <div className="section-inverse">
+          <div className="container">
+            <div className="d-flex justify-content-center">
+              <h4 className="footer-title float-title">
+                Porque somos importantes?
+              </h4>
+            </div>
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="row no-gutters">
+                  <div className="col-md-6">
+                    <p className="featured-box">
+                      As redes sociais estão inundadas de fake news.
+                    </p>
+                  </div>
+                  <div className="col-md-6">
+                    <p className="featured-box">
+                      São quase 26 mil candidatos divididos em 35 partidos.
+                    </p>
+                  </div>
+                  <div className="col-md-6">
+                    <p className="featured-box">
+                      Apenas para a Câmara dos Deputados serão mais de oito mil
+                      candidaturas.
+                    </p>
+                  </div>
+                  <div className="col-md-6">
+                    <p className="featured-box">
+                      Muitas delas de parlamentares que tentam a reeleição.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="featured">
+                  <div className="row no-gutters">
+                    <div className="col-md-6">
+                      <img
+                        src={require("../../data/img/people.png")}
+                        alt="Ícones representando candidatos destacando nove entre dez"
+                        width="100%"
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <p className="highlight-box">
+                        Nove em cada 10 deputados querem permanecer no
+                        Legislativo.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section className="intro">
+          <div className="container">
+            <div className="d-flex justify-content-center">
+              <h4 className="footer-title">Parceiros</h4>
+            </div>
+
+            <div className="row">
+              <div className="col-4">
+                <ul class="ul">
+                  <li>Cidade Escola Aprendiz</li>
+                  <li>ClimaInfo</li>
+                  <li>Conectas</li>
+                  <li>Engajamundo</li>
+                  <li>Fundación Avina</li>
+                  <li>GIFE</li>
+                </ul>
+              </div>
+              <div className="col-4">
+                <ul class="ul">
+                  <li>ICE</li>
+                  <li>Idesam</li>
+                  <li>Impact Hub</li>
+                  <li>Inesc</li>
+                  <li>InPacto</li>
+                  <li>Instituto Clima e Sociedade</li>
+                </ul>
+              </div>
+              <div className="col-4">
+                <ul class="ul">
+                  <li>Instituto Ethos</li>
+                  <li>Instituto Terroá </li>
+                  <li>Observatório do Clima</li>
+                  <li>Sistema B</li>
+                  <li>Transparência Internacional </li>
+                  <li>Update Politics </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="intro">
+          <div className="container">
+            <div className="d-flex justify-content-center">
+              <h4 className="footer-title">Politica de Privacidade</h4>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nemo
+              deserunt et maiores dolorem, laboriosam animi, facilis laborum
+              obcaecati ipsa earum exercitationem neque. Molestiae cupiditate
+              quibusdam mollitia quae molestias temporibus.
+            </p>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Sobre;
