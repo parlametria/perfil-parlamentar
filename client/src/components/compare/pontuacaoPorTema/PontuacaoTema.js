aefeb660e730e851911e93e501e10a35084520ba
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-09-04 13:55:33 -0300

    Integra score por temas
    Reducer de usuários agora instancia o array sem a propriedade lenght
    ajusta funções para serem reaproveitaveis - elimina o pop das chaves na função de comparação
    ajusta erro em calculo de score

--- client/src/components/compare/pontuacaoPorTema/PontuacaoTema.js
+++ client/src/components/compare/pontuacaoPorTema/PontuacaoTema.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
 
 import isEmpty from "../../../validation/is-empty";
+import Spinner from "../../common/Spinner";
 
 class PontuacaoTema extends Component {
-
   render() {
-    let score;
-    if (!isEmpty(this.scoreTema)) {
-      score = this.scoreTema["Meio Ambiente"];
-      console.log(score);
-
-      return (
-        <div>
-          {score.tema} + :
-          <br />
-          {score.score}
+    let pontuacoesTemas;
+    if (!isEmpty(this.props.scoreTema)) {
+      pontuacoesTemas = Object.keys(this.props.scoreTema).map(tema => (
+        <div key={tema} className={tema}>
+          {tema + ": " + Math.round(this.props.scoreTema[tema] * 100) + "%"}
         </div>
-      );
+      ));
+
+      return <div>{pontuacoesTemas}</div>;
     }
-    return null;
-  };
+    return <Spinner />;
+  }
 }
 
 export default PontuacaoTema;
--- client/src/reducers/usuarioReducer.js
+++ client/src/reducers/usuarioReducer.js
@@ -2,10 +2,17 @@ import { SET_SCORE_USUARIO } from "../actions/types";
 
 const TAM_PERGUNTAS = 46; // Esse tamanho é calculado em perguntasActions, mas não sei como inicializá-lo aqui e em que momento isso seria viável porque esse reducer é chamado antes da chamada ao banco. Então, acho melhor inicializar o array com um tamanho máximo.
 
+const inicializaRespostasUsuario = () => {
+  let respostasUsuario = [].fill.call({ length: TAM_PERGUNTAS }, 0);
+  delete respostasUsuario.length;
+
+  return respostasUsuario;
+};
+
 // userVotings: {id_votacao: voto}
 // arrayVotings: [0/1/-1]
 const initialState = {
-  respostasUsuario: [].fill.call({ length: TAM_PERGUNTAS }, 0),
+  respostasUsuario: inicializaRespostasUsuario(),
   arrayRespostasUsuario: Array(TAM_PERGUNTAS).fill(0),
   quantidadeVotos: 0
 };
