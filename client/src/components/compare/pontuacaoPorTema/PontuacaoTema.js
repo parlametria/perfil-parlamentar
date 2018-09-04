0ef95f0d2d025cd4af45d5181dfd516f16cab701
Jefferson Neves <jefferson.rpn@gmail.com>
2018-09-04 16:06:18 -0300

    Diminui espaçamento entre scores de temas

--- client/src/components/compare/pontuacaoPorTema/PontuacaoTema.js
+++ client/src/components/compare/pontuacaoPorTema/PontuacaoTema.js
@@ -10,18 +10,18 @@ class PontuacaoTema extends Component {
     let pontuacoesTemas;
     if (!isEmpty(this.props.scoreTema)) {
       pontuacoesTemas = Object.keys(this.props.scoreTema).map(tema => (
-        <div key={tema} className="score-theme-wrapper mb-3">
+        <div key={tema} className="score-theme-wrapper">
           <div>
             <span className="icon-minus strong" /> {tema}
           </div>
           <div className="row no-gutters d-flex align-items-center">
-            <div className="col-5 offset-2">
+            <div className="col-5 offset-2 col-lg-4">
               <div className="score-pie-chart">
                 <PieChart
                   data={[
                     {
                       value: 100 - Math.round(this.props.scoreTema[tema] * 100),
-                      color: "#cccccc"
+                      color: "#eeeeee"
                     },
                     {
                       value: Math.round(this.props.scoreTema[tema] * 100),
