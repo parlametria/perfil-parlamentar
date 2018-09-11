19cfc19d801b3672bfb21ba752817ec0630df0fb
Dandara Sousa <dandara.sousa@ccc.ufcg.edu.br>
2018-09-11 12:09:47 -0300

    início da tela de sou candidato

--- client/src/components/candidatos/SouCandidato.js
+++ client/src/components/candidatos/SouCandidato.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import CandidatosContainer from "./CandidatosContainer";
+import { Link } from "react-router-dom";
+
+import "./soucandidato.css";
+
 
 class SouCandidato extends Component {
   constructor(props) {
@@ -26,52 +29,57 @@ class SouCandidato extends Component {
     let candidato;
     return (
       <div className="container">
-        <div className="col-4">
+        <section className="intro-candidato">
+          <div className="container">
+            <h2 className="intro-title text-center">
+              É candidato e quer participar?
+          </h2>
+            <div className="my-3">
+              <Link to="/" className="btn btn-link">
+                <span className="icon-back" /> Voltar para o quiz
+          </Link>
+            </div>
+          </div>
+        </section>
+
+        <div className="col-md-4">
           <input
             type="text"
             className="form-control form-control-secondary"
-            placeholder="Pesquisar candidato/a..."
-            aria-label="Pesquisar candidato/a"
+            placeholder="Pesquisar CPF do/a candidato/a"
+            aria-label="Pesquisar CPF do/a candidato/a"
             aria-describedby="search-candidate"
           //onChange={this.buscaNome}
           //value={this.state.filtro.nome}
           />
         </div>
 
+        <div className="container">
+          <section className="sou-candidato">
+            <div className="grid-wrapper">
+              <div className="grid-main">
+                <section className="grid-panel panel-master">
+                  <div>
+                    Se você é um candidato e por algum motivo não recebeu o nosso e-mail entre em contato conosco
+                <a href="mailto:'contato@vozativa.org'"
+                      target="_blank"> através de uma mensagem.</a>
+                  </div>
+                </section>
+                <div className="grid-separator" />
+                <section className="grid-panel panel-detail">
+                  {candidato = this.candidato ? (this.candidato) : (
+                    <div>
+                      Pesquise pelo seu cpf
+                    </div>
+                  )}
+                </section>
+              </div>
 
-        <div className="row">
-          <div className="col-md-6">
-            {candidato = this.candidato ? this.candidato : "candidato não definido"}
-          </div>
-          <div className="col-md-6">
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                Nome:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-              </label>
-              <label>
-                Sobrenome:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-              </label>
-
-              <label>
-                E-mail:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-              </label>
-
-              <label>
-                Alguma sugestão ou comentário:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-
-              </label>
-
-              <input type="submit" value="Submit" />
-            </form>
-          </div>
-
-
+            </div>
+          </section>
         </div>
       </div>
+
     )
   }
 }
