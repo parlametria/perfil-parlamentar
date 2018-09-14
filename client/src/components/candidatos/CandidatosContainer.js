39e2a27f35e4de2a14714cf12596b9191640da9e
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-09-13 23:00:26 -0300

    Corrige lentidão na busca por nome

--- client/src/components/candidatos/CandidatosContainer.js
+++ client/src/components/candidatos/CandidatosContainer.js
@@ -316,13 +316,13 @@ class CandidatosContainer extends Component {
                     {candidatos.slice(paginacao.inicio, paginacao.final)}
                   </FlipMove>
                 </div>
-                <div className="candidatos-pagination d-flex justify-content-center flex-wrap mb-3">
+                {!isFiltrandoPorNome ? <div className="candidatos-pagination d-flex justify-content-center flex-wrap mb-3">
                   {paginacao.inicio !== 0 ? btnFirst : null}
                   {paginacao.inicio > 0 ? btnMenosCandidatos : null}
                   {paginacao.final < paginacao.totalCandidatos
                     ? btnMaisCandidatos
                     : null}
-                </div>
+                </div> : null}
               </div>
             )}
         </div>
@@ -353,18 +353,6 @@ class CandidatosContainer extends Component {
         </FlipMove>
       </div>
     );
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.subscription = this.onSearch$
-      .debounceTime(DEBOUNCE_TIME)
-      .subscribe(debounced => {
-        nextProps.setCandidatosFiltrados();
-        this.setState({
-          debounced,
-          isPesquisando: false
-        });
-      });
   }
 
   componentDidMount() {
