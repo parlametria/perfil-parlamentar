643a3b20bbf86906f9be0e91fc8cdbe84943bc9a
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-11-01 12:54:07 -0300

    Adiciona novos campos de configuração

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,7 @@ module.exports = {
   mongoURI: process.env.MONGODB_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
   facebookAppID: process.env.FACEBOOK_APP_ID,
-  facebookAppSecret: process.env.FACEBOOK_APP_SECRET
+  facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:5000/api/auth/facebook/callback",
+  profileURL: "https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email",
 };
