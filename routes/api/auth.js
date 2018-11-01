a0ae9897aa832a0a8e9bda5aa42a29abdedafaac
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-11-01 12:57:18 -0300

    Cria rota de login com fb

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const { generateToken, sendToken } = require("../../utils/token.utils");
 
-//const passport = require("../../config/passport");
 const passport = require("passport");
+require('../../config/passport')();
 
-// @route   GET api/auth/facebook
+// @route   POST api/auth/facebook
 // @desc    Login com facebook
 // @access  Public
-router.get("/facebook", (req, res) => {
-  return passport.authenticate("facebook");
-});
+router.post("/facebook", passport.authenticate("facebook-token", { session: false }), (req, res, next) => {
+  if (!req.user) {
+    return res.send(401, 'User Not Authenticated');
+  }
+  req.auth = {
+    id: req.user.id
+  };
+
+  next();
+}, generateToken, sendToken);
 
 router.get("/callback/facebook", (req, res) => {
   return passport.authenticate("facebook", {
