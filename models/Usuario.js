eff97c0e54b90a7e0de15bd5872825a2130c6513
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-11-05 15:43:28 -0300

    Adiciona função para criação de usuário via twitter no modelo de dados

--- models/Usuario.js
+++ models/Usuario.js
@@ -27,67 +27,128 @@ const UsuarioSchema = new Schema({
       token: String
     },
     select: false
+  },
+  twitterProvider: {
+    type: {
+      id: String,
+      token: String
+    },
+    select: false
   }
 });
 
-UsuarioSchema.set('toJSON', { getters: true, virtuals: true });
+UsuarioSchema.set("toJSON", { getters: true, virtuals: true });
 
-UsuarioSchema.statics.upsertFbUser = function (accessToken, refreshToken, profile, cb) {
+UsuarioSchema.statics.upsertFbUser = function(
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) {
   const that = this;
-  return this.findOne({
-    'facebookProvider.id': profile.id
-  }, function (err, user) {
-    // no user was found, lets create a new one
-    if (!user) {
-      const newUser = new that({
-        firstName: profile.displayName.split(" ")[0],
-        fullName: profile.displayName,
-        email: profile.emails[0].value,
-        facebookProvider: {
-          id: profile.id,
-          token: accessToken
-        }
-      });
+  return this.findOne(
+    {
+      "facebookProvider.id": profile.id
+    },
+    function(err, user) {
+      // no user was found, lets create a new one
+      if (!user) {
+        const newUser = new that({
+          firstName: profile.displayName.split(" ")[0],
+          fullName: profile.displayName,
+          email: profile.emails[0].value,
+          facebookProvider: {
+            id: profile.id,
+            token: accessToken
+          }
+        });
 
-      newUser.save(function (error, savedUser) {
-        if (error) {
-          console.log(error);
-        }
-        return cb(error, savedUser);
-      });
-    } else {
-      return cb(err, user);
+        newUser.save(function(error, savedUser) {
+          if (error) {
+            console.log(error);
+          }
+          return cb(error, savedUser);
+        });
+      } else {
+        return cb(err, user);
+      }
     }
-  });
+  );
 };
 
-UsuarioSchema.statics.upsertGoogleUser = function (accessToken, refreshToken, profile, cb) {
+UsuarioSchema.statics.upsertGoogleUser = function(
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) {
   const that = this;
-  return this.findOne({
-    'googleProvider.id': profile.id
-  }, function (err, user) {
-    // no user was found, lets create a new one
-    if (!user) {
-      const newUser = new that({
-        firstName: profile.displayName.split(" ")[0],
-        fullName: profile.displayName,
-        email: profile.emails[0].value,
-        googleProvider: {
-          id: profile.id,
-          token: accessToken
-        }
-      });
+  return this.findOne(
+    {
+      "googleProvider.id": profile.id
+    },
+    function(err, user) {
+      // no user was found, lets create a new one
+      if (!user) {
+        const newUser = new that({
+          firstName: profile.displayName.split(" ")[0],
+          fullName: profile.displayName,
+          email: profile.emails[0].value,
+          googleProvider: {
+            id: profile.id,
+            token: accessToken
+          }
+        });
 
-      newUser.save(function (error, savedUser) {
-        if (error) {
-          console.log(error);
-        }
-        return cb(error, savedUser);
-      });
-    } else {
-      return cb(err, user);
+        newUser.save(function(error, savedUser) {
+          if (error) {
+            console.log(error);
+          }
+          return cb(error, savedUser);
+        });
+      } else {
+        return cb(err, user);
+      }
     }
-  });
+  );
+};
+
+UsuarioSchema.statics.upsertTwitterUser = function(
+  token,
+  tokenSecret,
+  profile,
+  cb
+) {
+  var that = this;
+  return this.findOne(
+    {
+      "twitterProvider.id": profile.id
+    },
+    function(err, user) {
+      // no user was found, lets create a new one
+      if (!user) {
+        var newUser = new that({
+          firstName: profile.displayName.split(" ")[0],
+          fullName: profile.displayName,
+          email: profile.emails[0].value,
+          twitterProvider: {
+            id: profile.id,
+            token: token,
+            tokenSecret: tokenSecret
+          }
+        });
+
+        newUser.save(function(error, savedUser) {
+          if (error) {
+            console.log(error);
+          }
+          return cb(error, savedUser);
+        });
+      } else {
+        return cb(err, user);
+      }
+    }
+  );
 };
 
 module.exports = Usuario = mongoose.model("usuarios", UsuarioSchema);
