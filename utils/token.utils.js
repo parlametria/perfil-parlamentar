5ad0581d455ff04a78caa1c31e0f77de1b8c72d3
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-11-08 17:31:52 -0300

    Adiciona mais detalhes no token jwt

--- utils/token.utils.js
+++ utils/token.utils.js
@@ -1,22 +1,28 @@
 const jwt = require("jsonwebtoken");
 const keys = require("../config/keys");
 
-const createToken = function (auth) {
-  return jwt.sign({
-    id: auth.id
-  }, keys.secretOrKey,
+const createToken = function(auth) {
+  return jwt.sign(
+    {
+      id: auth.id,
+      firstName: auth.firstName,
+      photo: auth.photo
+    },
+    keys.secretOrKey,
     {
       expiresIn: 60 * 120
-    });
+    }
+  );
 };
 
 module.exports = {
-  generateToken: function (req, res, next) {
+  generateToken: function(req, res, next) {
     req.token = createToken(req.auth);
     return next();
   },
-  sendToken: function (req, res) {
-    res.setHeader('x-auth-token', req.token);
+  sendToken: function(req, res) {
+    res.setHeader("x-auth-token", req.token);
     return res.status(200).send(JSON.stringify(req.user));
-  }
+  },
+  createToken
 };
