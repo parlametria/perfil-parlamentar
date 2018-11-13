4b03a3bdc24ff8a83c5642ef6f67399c2db9e950
luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2018-11-13 17:28:22 -0300

    Altera tabela de temas para ficar de acordo com o csv criado

--- models/tema.js
+++ models/tema.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize, type) => {
   return sequelize.define("Tema", {
+    tema: type.STRING,
     id: {
       type: type.INTEGER,
       primaryKey: true
-    },
-    nome: type.STRING
+    }
   });
 };
