22967ec1d2614aa6009a532f1ed76154446a4ef2
luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2018-11-16 16:54:49 -0300

    Altera primary key de proposicoes

--- models/Prop.js
+++ models/Prop.js
@@ -1,11 +1,11 @@
 module.exports = (sequelize, type) => {
   return sequelize.define("Proposicao", {
-    id: {
+    id: type.INTEGER,
+    projeto_lei: type.STRING,
+    id_votacao: {
       type: type.INTEGER,
       primaryKey: true
     },
-    projeto_lei: type.STRING,
-    id_votacao: type.INTEGER,
     titulo: type.STRING,
     descricao: type.STRING,
     tema_id: type.INTEGER
