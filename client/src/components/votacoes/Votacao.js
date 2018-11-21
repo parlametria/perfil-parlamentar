1ca2fba6a55392eb2b0041b92c329811fb027a5e
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-11-21 15:58:06 -0300

    Inicia encapsulamento de pergunta em votação

---
+++ client/src/components/votacoes/Votacao.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Pergunta from "../perguntas/Pergunta";
+
+class Votacao extends Component {
+  render() {
+    return <Pergunta />;
+  }
+}
+
+export default Votacao;
