ecb42a2675a87858d74bc5082c1df9b9afeb3471
luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2018-12-11 18:13:35 -0300

    Atualiza modelo de usuario

--- models/postgres/usuario.js
+++ models/postgres/usuario.js
@@ -1,22 +1,36 @@
 module.exports = (sequelize, type) => {
-  return sequelize.define(
+  usuario = sequelize.define(
     "usuario",
     {
       first_name: type.STRING,
       full_name: type.STRING,
       email: type.STRING,
       photo: type.STRING,
-      facebook: type.STRING,
-      twitter: type.STRING,
-      google: type.STRING,
       id: {
         type: type.INTEGER,
         autoIncrement: true,
         primaryKey: true
-      }
+      },
+
+      provider: type.STRING,
+      provider_id: type.STRING,
+      token: type.STRING
     },
     {
       timestamps: false
     }
   );
+
+  usuario.associate = function(models) {
+    usuario.hasMany(models.respostau, {
+      foreignKey: "user_id",
+      as: "user_resp"
+    }),
+      usuario.hasMany(models.votacaou, {
+        foreignKey: "user_id",
+        as: "user_vot"
+      });
+  };
+
+  return usuario;
 };
