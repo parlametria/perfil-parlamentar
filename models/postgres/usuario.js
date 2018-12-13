71a2069d7410d12c7a652efaca726a0af1c91061
luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2018-12-13 16:30:52 -0300

    funcoes de upsert de usuario adicionadas

--- models/postgres/usuario.js
+++ models/postgres/usuario.js
@@ -32,5 +32,76 @@ module.exports = (sequelize, type) => {
       });
   };
 
+  usuario.upsertGoogleUser = function(
+    accessToken,
+    refreshToken,
+    profile,
+    respostas,
+    cb
+  ) {
+    const that = this;
+    return this.findOne({
+      procider: "google",
+      provider_id: profile.id
+    }).then((user, err) => {
+      // no user was found, lets create a new one
+      if (!user) {
+        const newUser = new that({
+          first_name: profile.displayName.split(" ")[0],
+          full_name: profile.displayName,
+          email: profile.emails[0].value,
+          photo: profile._json.picture,
+          provider: "google",
+          provider_id: profile.id,
+          token: accessToken
+        });
+        newUser.save().then((savedUser, error) => {
+          if (error) {
+            console.log(error);
+          }
+          return cb(savedUser, error);
+        });
+      } else {
+        return cb(user, err);
+      }
+    });
+  };
+
+  usuario.upsertFbUser = function(
+    accessToken,
+    refreshToken,
+    profile,
+    respostas,
+    cb
+  ) {
+    const that = this;
+    return this.findOne({
+      provider: "facebook",
+      provider_id: profile.id
+    }).then((user, err) => {
+      // no user was found, lets create a new one
+      if (!user) {
+        const newUser = new that({
+          firstName: profile.displayName.split(" ")[0],
+          fullName: profile.displayName,
+          email: profile.emails[0].value,
+          photo: profile.photos[0].value,
+          provider: "facebook",
+          provider_id: profile.id,
+          token: accessToken
+        });
+
+        newUser.save().then((savedUser, error) => {
+          if (error) {
+            console.log(error);
+          }
+          return cb(savedUser, error);
+        });
+      } else {
+        return cb(user, err);
+      }
+    });
+  };
+
   return usuario;
 };
