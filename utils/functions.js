f5811b69f926cef4ed6d03554ba00df2856de05c
luiza Carvalho <luiza.silveira@ccc.ufcg.edu.br>
2018-12-13 17:32:58 -0300

    adiciona funcao de modificar respostas do usuario

--- utils/functions.js
+++ utils/functions.js
@@ -25,4 +25,18 @@ function formataVotacoes(resultado) {
   });
   return resultadoNovo;
 }
-module.exports = { formataRespostas, formataVotacoes };
+
+function formataRespostasUser(resultado) {
+  const resultadoNovo = resultado.map(cand => cand.get({ plain: true }));
+
+  resultadoNovo.forEach(cand => {
+    const respostas = {};
+    cand.user_resp.forEach(resposta => {
+      respostas[resposta.pergunta_id] = resposta.resposta;
+    });
+    cand.respostas = respostas;
+    delete cand.user_resp;
+  });
+  return resultadoNovo;
+}
+module.exports = { formataRespostas, formataVotacoes, formataRespostasUser };
