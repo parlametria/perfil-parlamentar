7f2c010da1b4261d9bcff8550f1a26bca556e2ac
gileadekelvin <gileadekelvin@gmail.com>
2019-02-11 15:46:27 -0300

    Adiciona serviço de autorização de rota na seção de providers

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,18 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { PerguntaService } from './shared/services/pergunta.service';
-import { LoginService } from './shared/services/login.service';
-import { getAuthServiceConfigs } from "./shared/config/socialLoginConfig";
-import { TokenInterceptor } from "./shared/auth/token.interceptor";
 import { LoginComponent } from './user/login/login.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
+import { PerguntaService } from './shared/services/pergunta.service';
+import { LoginService } from './shared/services/login.service';
+import { AuthGuardService } from './shared/services/auth-guard.service';
+
+import { getAuthServiceConfigs } from "./shared/config/socialLoginConfig";
+import { TokenInterceptor } from "./shared/auth/token.interceptor";
+
 
 @NgModule({
   declarations: [
@@ -28,6 +33,7 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
   providers: [
     PerguntaService,
     LoginService,
+    AuthGuardService,
     {
       provide: AuthServiceConfig,
       useFactory: getAuthServiceConfigs
