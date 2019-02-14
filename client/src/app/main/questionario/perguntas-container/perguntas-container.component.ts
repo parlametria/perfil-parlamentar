818fe4b391922c50f467e2484e9e3cb002ee5c35
gileadekelvin <gileadekelvin@gmail.com>
2019-02-14 17:54:39 -0300

    Pula para pergunta seguinte após responder a pergunta atual Adiciona função para ordenação personalizada das votações no questionário

--- client/src/app/main/questionario/perguntas-container/perguntas-container.component.ts
+++ client/src/app/main/questionario/perguntas-container/perguntas-container.component.ts
@@ -19,13 +19,14 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
   readonly FAVOR = 1;
   readonly NAOSEI = -2;
   readonly CONTRA = -1;
-  
+
   private unsubscribe = new Subject();
 
   temaSelecionado: string;
 
   listaTemas: Tema[];
   listaProposicoes: Proposicao[];
+  todasProposicoesOrdenadas: Proposicao[]
 
   perguntasTemaSelecionado: Proposicao[];
   perguntaSelecionada: Proposicao;
@@ -77,7 +78,13 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
     this.perguntaService.getProposicoes().pipe(takeUntil(this.unsubscribe)).subscribe(
       proposicoes => {
         this.listaProposicoes = proposicoes;
-        this.perguntasTemaSelecionado = this.listaProposicoes.filter((proposicao) => proposicao.tema_id === Number(this.temaSelecionado));
+
+        // Filtra as perguntas do tema selecionado e ordena pelo critério: id da votação
+        this.perguntasTemaSelecionado = this.sortProposicoes(this.listaProposicoes.filter(
+          (proposicao) => proposicao.tema_id === Number(this.temaSelecionado)));
+
+        this.todasProposicoesOrdenadas = this.sortProposicoesUsingArray(this.listaProposicoes, this.receivedTemas);
+
         this.perguntaSelecionada = this.perguntasTemaSelecionado[0];
       },
       error => console.log(error)
@@ -100,12 +107,13 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
     }
 
     this.userService.setResposta(novaResposta);
+    this.proximaPergunta();
   }
 
   onTemaChange() {
-    this.perguntasTemaSelecionado = this.listaProposicoes.filter((proposicao) => {
+    this.perguntasTemaSelecionado = this.sortProposicoes(this.listaProposicoes.filter((proposicao) => {
       return proposicao.tema_id === Number(this.temaSelecionado);
-    });
+    }));
 
     this.perguntaSelecionada = this.perguntasTemaSelecionado[0];
   }
@@ -116,7 +124,30 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
     })[0];
   }
 
-  respondeuPergunta(idVotacao) {    
+  proximaPergunta() {
+    let index = this.todasProposicoesOrdenadas.indexOf(this.perguntaSelecionada);
+    console.log(index);
+    if (index === this.todasProposicoesOrdenadas.length - 1) {
+      // Usuário finalizou questionário      
+      this.temaSelecionado = this.receivedTemas[0].toString();
+      this.onTemaChange();
+
+    } else {
+      // Passa para próxima pergunta
+      let proximaPergunta = this.todasProposicoesOrdenadas[index + 1];
+
+      // Checa se a próxima pergunta é do mesmo tema ou não
+      if (proximaPergunta.tema_id !== this.perguntaSelecionada.tema_id) {
+        this.temaSelecionado = proximaPergunta.tema_id.toString();
+        this.onTemaChange();
+      } else {
+        this.escolhePergunta(proximaPergunta.id_votacao);
+      }
+
+    }
+  }
+
+  respondeuPergunta(idVotacao) {
     return this.respostasUser.votacoes[idVotacao] === this.FAVOR ||
       this.respostasUser.votacoes[idVotacao] === this.CONTRA ||
       this.respostasUser.votacoes[idVotacao] === this.NAOSEI
@@ -132,6 +163,50 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
 
   respostaNeutra() {
     return this.respostasUser.votacoes[this.perguntaSelecionada.id_votacao] === -2;
+  }
+
+  sortObjectUsingArray(object, array, key) {
+    let objectOrdered = object.sort((a, b) => {
+      let A = Number(a[key]), B = Number(b[key]);
+
+      if (array.indexOf(A) > array.indexOf(B))
+        return 1;
+      else
+        return -1;
+
+    });
+
+    return objectOrdered
+  }
+
+  sortProposicoes(proposicoes) {
+    return proposicoes.sort((a, b) => {
+      if (a.id_votacao > b.id_votacao)
+        return 1
+      else if (a.id_votacao < b.id_votacao)
+        return -1
+      else
+        return 0
+    });
+  }
+
+  sortProposicoesUsingArray(proposicoes, arrayTemas) {
+    let proposicoesOrdered = proposicoes.sort((a, b) => {
+      let A = Number(a['tema_id']), B = Number(b['tema_id']);
+
+      if (arrayTemas.indexOf(A) > arrayTemas.indexOf(B))
+        return 1;
+      else if (arrayTemas.indexOf(A) < arrayTemas.indexOf(B))
+        return -1;
+      else if (a.id_votacao > b.id_votacao)
+        return 1
+      else if (a.id_votacao < b.id_votacao)
+        return -1
+      else
+        return 0
+    });
+
+    return proposicoesOrdered;
   }
 
   ngOnDestroy() {
