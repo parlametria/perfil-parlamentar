1bcaeb5915aeee5b06f71650c3f8c746f23a0792
gileadekelvin <gileadekelvin@gmail.com>
2019-03-11 13:27:07 -0300

    Resolve bug no cálculo do match

--- client/src/actions/candidatosActions.js
+++ client/src/actions/candidatosActions.js
@@ -50,10 +50,10 @@ export const comparaRespostas = (
   idsVozAtiva.forEach(idPergunta => {
     respostasIguais +=
       respostasCandidatos[idPergunta] !== undefined &&
-      respostasCandidatos[idPergunta] !== null &&
-      respostasUsuarioVozAtiva[idPergunta] !== 0 &&
-      respostasUsuarioVozAtiva[idPergunta] !== -2 &&
-      respostasCandidatos[idPergunta] === respostasUsuarioVozAtiva[idPergunta]
+        respostasCandidatos[idPergunta] !== null &&
+        respostasUsuarioVozAtiva[idPergunta] !== 0 &&
+        respostasUsuarioVozAtiva[idPergunta] !== -2 &&
+        respostasCandidatos[idPergunta] === respostasUsuarioVozAtiva[idPergunta]
         ? 1
         : 0;
   });
@@ -62,10 +62,10 @@ export const comparaRespostas = (
   idsVotacoes.forEach(idPergunta => {
     respostasIguais +=
       votacoesCandidatos[idPergunta] !== undefined &&
-      votacoesCandidatos[idPergunta] !== null &&
-      respostasUsuarioVotacoes[idPergunta] !== 0 &&
-      respostasUsuarioVotacoes[idPergunta] !== -2 &&
-      votacoesCandidatos[idPergunta] === respostasUsuarioVotacoes[idPergunta]
+        votacoesCandidatos[idPergunta] !== null &&
+        respostasUsuarioVotacoes[idPergunta] !== 0 &&
+        respostasUsuarioVotacoes[idPergunta] !== -2 &&
+        votacoesCandidatos[idPergunta] === respostasUsuarioVotacoes[idPergunta]
         ? 1
         : 0;
   });
@@ -141,9 +141,9 @@ const verificaQuantidadeVotos = (
 const contPerguntasRespondidasComuns = (perguntas, candidato) => {
   let cont = 0;
   perguntas.forEach(pergunta => {
-    if(candidato.respostas[pergunta] === 1 || 
+    if (candidato.respostas[pergunta] === 1 ||
       candidato.respostas[pergunta] === -1) {
-        cont = cont + 1;
+      cont = cont + 1;
     }
   });
   return cont;
@@ -152,11 +152,11 @@ const contPerguntasRespondidasComuns = (perguntas, candidato) => {
 const contVotacoesRespondidasComuns = (votacoes, candidato) => {
   let cont = 0;
   votacoes.forEach(votacao => {
-    if(candidato[votacao] === 1 ||
+    if (candidato[votacao] === 1 ||
       candidato[votacao] === -1) {
-        cont = cont + 1;
+      cont = cont + 1;
     }
-  }); 
+  });
   return cont;
 };
 
@@ -172,16 +172,16 @@ const calculaNumRespostasConsideradas = (
   let numRespostasConsideradas;
   if (!naoRespondeuCamara && !naoRespondeuVozAtiva) {
     numRespostasConsideradas = contPerguntasRespondidasComuns(perguntasRespondidas, respostasCandidato);
-    numRespostasConsideradas = numRespostasConsideradas + 
-    contVotacoesRespondidasComuns(votacoesRespondidas, votacoesCandidato);
+    numRespostasConsideradas = numRespostasConsideradas +
+      contVotacoesRespondidasComuns(votacoesRespondidas, votacoesCandidato);
 
   } else if (naoRespondeuCamara) {
-      if (perguntasRespondidas.length === 0) {
-        numRespostasConsideradas = 1;
-      } else {
-        numRespostasConsideradas = contPerguntasRespondidasComuns(perguntasRespondidas, respostasCandidato);
-      } 
-  } else if (naoRespondeuVozAtiva){
+    if (perguntasRespondidas.length === 0) {
+      numRespostasConsideradas = 1;
+    } else {
+      numRespostasConsideradas = contPerguntasRespondidasComuns(perguntasRespondidas, respostasCandidato);
+    }
+  } else if (naoRespondeuVozAtiva) {
     if (votacoesRespondidas.length === 0) {
       numRespostasConsideradas = 1;
     } else {
@@ -191,6 +191,7 @@ const calculaNumRespostasConsideradas = (
   return numRespostasConsideradas;
 }
 export const calculaScore = dadosPergunta => dispatch => {
+  console.log(dadosPergunta);
   if (dadosPergunta) dispatch(atualizaScore(dadosPergunta));
   else dispatch(calculaTodoScore());
 };
@@ -210,7 +211,7 @@ export const atualizaScore = ({ idPergunta, respostaAnterior }) => (
   );
 
   let { votosIguaisUsuarioCandidatos } = getState().candidatosReducer;
-  
+
   const respostasConsideradas = () => {
     let tipoPergunta;
     if (respostasUsuario.vozAtiva[idPergunta]) {
@@ -265,42 +266,58 @@ export const atualizaScore = ({ idPergunta, respostaAnterior }) => (
       )
         votosIguaisUsuarioCandidatos[elem] =
           votosIguaisUsuarioCandidatos[elem] + 1;
-    } else {            
+    } else {
       if (respostaAnterior === -1) {
         if (respostasConsideradasUsuario.respostasUsuario[idPergunta] === 1) {
           if (
             respostasConsideradasUsuario.respostasUsuario[idPergunta] ===
-            respostasCandidatosConsideradas[idPergunta]             
+            respostasCandidatosConsideradas[idPergunta]
           ) {
             votosIguaisUsuarioCandidatos[elem] =
-              votosIguaisUsuarioCandidatos[elem] + 1;              
+              votosIguaisUsuarioCandidatos[elem] + 1;
           } else {
             votosIguaisUsuarioCandidatos[elem] =
-              votosIguaisUsuarioCandidatos[elem] - 1;              
+              votosIguaisUsuarioCandidatos[elem] - 1;
+          }
+        } else if (respostasConsideradasUsuario.respostasUsuario[idPergunta] === -2) {
+          if (
+            respostaAnterior ===
+            respostasCandidatosConsideradas[idPergunta]
+          ) {
+            votosIguaisUsuarioCandidatos[elem] =
+              votosIguaisUsuarioCandidatos[elem] - 1;
           }
         }
       } else {
         if (respostasConsideradasUsuario.respostasUsuario[idPergunta] === -1) {
           if (
             respostasConsideradasUsuario.respostasUsuario[idPergunta] ===
-            respostasCandidatosConsideradas[idPergunta]             
+            respostasCandidatosConsideradas[idPergunta]
           ) {
             votosIguaisUsuarioCandidatos[elem] =
-              votosIguaisUsuarioCandidatos[elem] + 1;              
+              votosIguaisUsuarioCandidatos[elem] + 1;
           } else {
             votosIguaisUsuarioCandidatos[elem] =
-              votosIguaisUsuarioCandidatos[elem] - 1;    
+              votosIguaisUsuarioCandidatos[elem] - 1;
+          }
+        } else if (respostasConsideradasUsuario.respostasUsuario[idPergunta] === -2) {
+          if (
+            respostaAnterior ===
+            respostasCandidatosConsideradas[idPergunta]
+          ) {
+            votosIguaisUsuarioCandidatos[elem] =
+              votosIguaisUsuarioCandidatos[elem] - 1;
           }
         }
       }
-    } 
+    }
 
     if (numRespostasConsideradas === 0) {
-      scoreCandidatos[elem] = 0;      
+      scoreCandidatos[elem] = 0;
     } else {
       scoreCandidatos[elem] = votosIguaisUsuarioCandidatos[elem] / numRespostasConsideradas;
     }
-          
+
   });
 
   dispatch({
@@ -319,9 +336,9 @@ export const calculaTodoScore = () => (dispatch, getState) => {
   const { respostasUsuario } = getState().usuarioReducer;
   const { dadosCandidatos } = getState().candidatosReducer;
   const { filtro } = getState().candidatosReducer;
-  const { votacoesCandidatos, dadosVotacoes } = getState().votacoesReducer;  
-  const { dadosPerguntas } = getState().perguntasReducer;   
-    
+  const { votacoesCandidatos, dadosVotacoes } = getState().votacoesReducer;
+  const { dadosPerguntas } = getState().perguntasReducer;
+
   const { idsVozAtiva, idsVotacoes } = filtraIdsPorTema(
     filtro.tema,
     dadosVotacoes,
@@ -369,8 +386,8 @@ export const calculaTodoScore = () => (dispatch, getState) => {
     );
 
     scoreCandidatos[elem] = respostasIguais / numRespostasConsideradas;
-    votosIguaisUsuarioCandidatos[elem] = respostasIguais;    
-  });  
+    votosIguaisUsuarioCandidatos[elem] = respostasIguais;
+  });
 
   dispatch({
     type: SET_SCORE_CANDIDATOS,
@@ -392,7 +409,7 @@ export const calculaScorePorTema = (
   const { abaAtiva } = getState().questionarioReducer;
   let perguntas;
 
-  if(abaAtiva === "Votacoes") {
+  if (abaAtiva === "Votacoes") {
     perguntas = getState().votacoesReducer.dadosVotacoes;
   } else {
     perguntas = getState().perguntasReducer.dadosPerguntas;
@@ -452,14 +469,14 @@ export const calculaScorePorTema = (
         respostasCandidatosTema[idPergunta] =
           dadosCandidato.respostas[idPergunta];
 
-        if(respostasCandidatosTema[idPergunta] &&
-          (respostasCandidatosTema[idPergunta] === 1 || 
-          respostasCandidatosTema[idPergunta] === -1) &&
+        if (respostasCandidatosTema[idPergunta] &&
+          (respostasCandidatosTema[idPergunta] === 1 ||
+            respostasCandidatosTema[idPergunta] === -1) &&
           respostasUsuario.vozAtiva[idPergunta] &&
           (respostasUsuario.vozAtiva[idPergunta] === 1 ||
-          respostasUsuario.vozAtiva[idPergunta] === -1)) {
-            numRespostasConsideradas = numRespostasConsideradas + 1;
-          }
+            respostasUsuario.vozAtiva[idPergunta] === -1)) {
+          numRespostasConsideradas = numRespostasConsideradas + 1;
+        }
       });
     }
 
@@ -467,22 +484,22 @@ export const calculaScorePorTema = (
       votacoesPorTema[tema].forEach(idVotacao => {
         votacoesCandidatosTema[idVotacao] =
           votacoesCandidatos[dadosCandidato.cpf][idVotacao];
-          
-        if(votacoesCandidatosTema[idVotacao] &&
+
+        if (votacoesCandidatosTema[idVotacao] &&
           (votacoesCandidatosTema[idVotacao] === 1 ||
-          votacoesCandidatosTema[idVotacao] === -1) &&
+            votacoesCandidatosTema[idVotacao] === -1) &&
           respostasUsuario.votacoes[idVotacao] &&
           (respostasUsuario.votacoes[idVotacao] === 1 ||
-          respostasUsuario.votacoes[idVotacao] === -1)) {
-            numRespostasConsideradas = numRespostasConsideradas + 1;
+            respostasUsuario.votacoes[idVotacao] === -1)) {
+          numRespostasConsideradas = numRespostasConsideradas + 1;
         }
       });
     }
 
-    numRespostasConsideradas = numRespostasConsideradas === 0 ? 
-    1 : numRespostasConsideradas;
+    numRespostasConsideradas = numRespostasConsideradas === 0 ?
+      1 : numRespostasConsideradas;
 
-    let score = comparaRespostas (
+    let score = comparaRespostas(
       dadosCandidato.cpf,
       respostasCandidatosTema,
       respostasUsuario.vozAtiva,
@@ -634,9 +651,9 @@ export const getDadosCandidatos = () => (dispatch, getState) => {
         axios
           .get(
             "/api/respostas/estados/" +
-              filtro.estado +
-              "/naoresponderam?pageNo=1&size=" +
-              ITENS_POR_REQ
+            filtro.estado +
+            "/naoresponderam?pageNo=1&size=" +
+            ITENS_POR_REQ
           )
           .then(respostas => {
             respostas.data.data.forEach(resp => {
@@ -710,29 +727,29 @@ export const getDadosCandidato = (
             let respostasCandidato =
               dadosCandidato.respostas[idPergunta];
 
-            if(respostasCandidato &&
-              (respostasCandidato === 1 || 
-              respostasCandidato === -1)) {
-                numRespostasConsideradas = numRespostasConsideradas + 1;
-              }
-          });
-        }
-    
-        if (votacoesRespondidas.length > 0) {
-          votacoesRespondidas.forEach(idVotacao => {
-            let respostasCandidato = 
-            dadosCandidato.votacoes[idVotacao];
-              
-            if(respostasCandidato &&
+            if (respostasCandidato &&
               (respostasCandidato === 1 ||
-              respostasCandidato === -1)) {
-                numRespostasConsideradas = numRespostasConsideradas + 1;
+                respostasCandidato === -1)) {
+              numRespostasConsideradas = numRespostasConsideradas + 1;
             }
           });
         }
-    
-        numRespostasConsideradas = numRespostasConsideradas === 0 ? 
-        1 : numRespostasConsideradas;
+
+        if (votacoesRespondidas.length > 0) {
+          votacoesRespondidas.forEach(idVotacao => {
+            let respostasCandidato =
+              dadosCandidato.votacoes[idVotacao];
+
+            if (respostasCandidato &&
+              (respostasCandidato === 1 ||
+                respostasCandidato === -1)) {
+              numRespostasConsideradas = numRespostasConsideradas + 1;
+            }
+          });
+        }
+
+        numRespostasConsideradas = numRespostasConsideradas === 0 ?
+          1 : numRespostasConsideradas;
 
         const score = comparaRespostas(
           idCandidato,
@@ -793,10 +810,10 @@ export const setCandidatosFiltrados = () => (dispatch, getState) => {
   axios
     .get(
       "api/respostas/estados/" +
-        filtro.estado +
-        "/partidos/" +
-        filtro.partido +
-        "/responderam"
+      filtro.estado +
+      "/partidos/" +
+      filtro.partido +
+      "/responderam"
     )
     .then(totalCandidatos =>
       dispatch({
@@ -863,9 +880,9 @@ export const setCandidatosFiltrados = () => (dispatch, getState) => {
         final: TAM_PAGINA,
         totalCandidatos:
           filtro.partido !== "Partidos" ||
-          filtro.nome !== "" ||
-          filtro.reeleicao !== "-1" ||
-          filtro.responderam !== "-1"
+            filtro.nome !== "" ||
+            filtro.reeleicao !== "-1" ||
+            filtro.responderam !== "-1"
             ? candidatos.length
             : candidatosRanqueados.length
       })
@@ -887,10 +904,10 @@ export const setPartidos = () => (dispatch, getState) => {
   axios
     .get(
       "api/respostas/estados/" +
-        filtro.estado +
-        "/partidos" +
-        "?eleito=" +
-        eleito
+      filtro.estado +
+      "/partidos" +
+      "?eleito=" +
+      eleito
     )
     .then(partidos => {
       dispatch({ type: SET_PARTIDOS, partidos: partidos.data.data });
@@ -921,11 +938,11 @@ export const getProximaPaginaCandidatos = () => (dispatch, getState) => {
   axios
     .get(
       "/api/respostas/estados/" +
-        filtro.estado +
-        "/naoresponderam?pageNo=" +
-        paginacao.paginaAtualAPI +
-        "&size=" +
-        ITENS_POR_REQ
+      filtro.estado +
+      "/naoresponderam?pageNo=" +
+      paginacao.paginaAtualAPI +
+      "&size=" +
+      ITENS_POR_REQ
     )
     .then(respostas => {
       respostas.data.data.forEach(resposta => {
