eba08fc08404effde9656e15fb4ea070114cb270
Jefferson Neves <jefferson.rpn@gmail.com>
2019-03-13 16:43:55 -0300

    Exibe botão para 'ver resultados' no questionário

--- client/src/app/main/questionario/perguntas-container/perguntas-container.component.ts
+++ client/src/app/main/questionario/perguntas-container/perguntas-container.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from "@angular/core";
 
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
-import { PerguntaService } from '../../../shared/services/pergunta.service';
-import { TemaService } from '../../../shared/services/tema.service';
-import { Tema } from '../../../shared/models/tema.model';
-import { Proposicao } from '../../../shared/models/proposicao.model';
-import { UserService } from '../../../shared/services/user.service';
-import { Resposta } from '../../../shared/models/resposta.model';
+import { PerguntaService } from "../../../shared/services/pergunta.service";
+import { TemaService } from "../../../shared/services/tema.service";
+import { Tema } from "../../../shared/models/tema.model";
+import { Proposicao } from "../../../shared/models/proposicao.model";
+import { UserService } from "../../../shared/services/user.service";
+import { Resposta } from "../../../shared/models/resposta.model";
 
 @Component({
-  selector: 'app-perguntas-container',
-  templateUrl: './perguntas-container.component.html',
-  styleUrls: ['./perguntas-container.component.scss']
+  selector: "app-perguntas-container",
+  templateUrl: "./perguntas-container.component.html",
+  styleUrls: ["./perguntas-container.component.scss"]
 })
 export class PerguntasContainerComponent implements OnInit, OnDestroy {
-
   readonly FAVOR = 1;
   readonly NAOSEI = -2;
   readonly CONTRA = -1;
+  readonly MIN_RESPOSTAS = 3;
 
   private unsubscribe = new Subject();
 
@@ -27,7 +27,7 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
 
   listaTemas: Tema[];
   listaProposicoes: Proposicao[];
-  todasProposicoesOrdenadas: Proposicao[]
+  todasProposicoesOrdenadas: Proposicao[];
 
   perguntasTemaSelecionado: Proposicao[];
   perguntaSelecionada: Proposicao;
@@ -40,70 +40,85 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
     private perguntaService: PerguntaService,
     private temaService: TemaService,
     private userService: UserService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.getTemas();
     this.getProposicoes();
     this.getRespostas();
-
   }
 
   getTemas() {
-    this.temaService.getTemas().pipe(takeUntil(this.unsubscribe)).subscribe(
-      temas => {
-        if (this.receivedTemas) {
-          this.listaTemas = this.sortObjectUsingArray(temas, this.receivedTemas, 'id');
+    this.temaService
+      .getTemas()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(
+        temas => {
+          if (this.receivedTemas) {
+            this.listaTemas = this.sortObjectUsingArray(
+              temas,
+              this.receivedTemas,
+              "id"
+            );
 
-          this.temaSelecionado = this.listaTemas[0].id;
-
-        } else {
-          this.listaTemas = temas;
-          this.temaSelecionado = '3';
-        }
-      },
-      error => console.log(error)
-    );
+            this.temaSelecionado = this.listaTemas[0].id;
+          } else {
+            this.listaTemas = temas;
+            this.temaSelecionado = "3";
+          }
+        },
+        error => console.log(error)
+      );
   }
 
   getProposicoes() {
-    this.perguntaService.getProposicoes().pipe(takeUntil(this.unsubscribe)).subscribe(
-      proposicoes => {
-        this.listaProposicoes = this.sortProposicoes(proposicoes);
+    this.perguntaService
+      .getProposicoes()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(
+        proposicoes => {
+          this.listaProposicoes = this.sortProposicoes(proposicoes);
 
-        // Filtra as perguntas do tema selecionado
-        this.perguntasTemaSelecionado = this.listaProposicoes.filter(
-          (proposicao) => proposicao.tema_id === Number(this.temaSelecionado));
+          // Filtra as perguntas do tema selecionado
+          this.perguntasTemaSelecionado = this.listaProposicoes.filter(
+            proposicao => proposicao.tema_id === Number(this.temaSelecionado)
+          );
 
-        this.todasProposicoesOrdenadas = this.sortProposicoesUsingArray(this.listaProposicoes, this.receivedTemas);
+          this.todasProposicoesOrdenadas = this.sortProposicoesUsingArray(
+            this.listaProposicoes,
+            this.receivedTemas
+          );
 
-        this.perguntaSelecionada = this.perguntasTemaSelecionado[0];
-      },
-      error => console.log(error)
-    );
+          this.perguntaSelecionada = this.perguntasTemaSelecionado[0];
+        },
+        error => console.log(error)
+      );
   }
 
   getRespostas() {
-    this.userService.getRespostas().pipe(takeUntil(this.unsubscribe)).subscribe(
-      res => {
-        this.respostasUser = res;
-      },
-      error => console.log(error)
-    );
+    this.userService
+      .getRespostas()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(
+        res => {
+          this.respostasUser = res;
+        },
+        error => console.log(error)
+      );
   }
 
   setResposta(resposta) {
     let novaResposta = {
       id_votacao: this.perguntaSelecionada.id_votacao,
       resposta: resposta
-    }
+    };
 
     this.userService.setResposta(novaResposta);
     this.proximaPergunta();
   }
 
   onTemaChange() {
-    this.perguntasTemaSelecionado = this.listaProposicoes.filter((proposicao) => {
+    this.perguntasTemaSelecionado = this.listaProposicoes.filter(proposicao => {
       return proposicao.tema_id === Number(this.temaSelecionado);
     });
 
@@ -111,19 +126,22 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
   }
 
   escolhePergunta(idVotacao) {
-    this.perguntaSelecionada = this.perguntasTemaSelecionado.filter((pergunta) => {
-      return pergunta.id_votacao === idVotacao
-    })[0];
+    this.perguntaSelecionada = this.perguntasTemaSelecionado.filter(
+      pergunta => {
+        return pergunta.id_votacao === idVotacao;
+      }
+    )[0];
   }
 
   proximaPergunta() {
-    let index = this.todasProposicoesOrdenadas.indexOf(this.perguntaSelecionada);
+    let index = this.todasProposicoesOrdenadas.indexOf(
+      this.perguntaSelecionada
+    );
 
     if (index === this.todasProposicoesOrdenadas.length - 1) {
-      // Usuário finalizou questionário      
+      // Usuário finalizou questionário
       this.temaSelecionado = this.receivedTemas[0].toString();
       this.onTemaChange();
-
     } else {
       // Passa para próxima pergunta
       let proximaPergunta = this.todasProposicoesOrdenadas[index + 1];
@@ -135,80 +153,87 @@ export class PerguntasContainerComponent implements OnInit, OnDestroy {
       } else {
         this.escolhePergunta(proximaPergunta.id_votacao);
       }
-
     }
   }
 
   respondeuPergunta(idVotacao) {
     if (this.respostasUser.votacoes) {
-      return this.respostasUser.votacoes[idVotacao] === this.FAVOR ||
+      return (
+        this.respostasUser.votacoes[idVotacao] === this.FAVOR ||
         this.respostasUser.votacoes[idVotacao] === this.CONTRA ||
         this.respostasUser.votacoes[idVotacao] === this.NAOSEI
+      );
     }
   }
 
   respostaPositiva() {
     if (this.respostasUser.votacoes)
-      return this.respostasUser.votacoes[this.perguntaSelecionada.id_votacao] === this.FAVOR;
+      return (
+        this.respostasUser.votacoes[this.perguntaSelecionada.id_votacao] ===
+        this.FAVOR
+      );
   }
 
   respostaNegativa() {
     if (this.respostasUser.votacoes)
-      return this.respostasUser.votacoes[this.perguntaSelecionada.id_votacao] === this.CONTRA;
+      return (
+        this.respostasUser.votacoes[this.perguntaSelecionada.id_votacao] ===
+        this.CONTRA
+      );
   }
 
   respostaNeutra() {
     if (this.respostasUser.votacoes)
-      return this.respostasUser.votacoes[this.perguntaSelecionada.id_votacao] === this.NAOSEI;
+      return (
+        this.respostasUser.votacoes[this.perguntaSelecionada.id_votacao] ===
+        this.NAOSEI
+      );
   }
 
   sortObjectUsingArray(object, array, key) {
     let objectOrdered = object.sort((a, b) => {
-      let A = Number(a[key]), B = Number(b[key]);
+      let A = Number(a[key]),
+        B = Number(b[key]);
 
-      if (array.indexOf(A) > array.indexOf(B))
-        return 1;
-      else
-        return -1;
-
+      if (array.indexOf(A) > array.indexOf(B)) return 1;
+      else return -1;
     });
 
-    return objectOrdered
+    return objectOrdered;
   }
 
   sortProposicoes(proposicoes) {
     return proposicoes.sort((a, b) => {
-      if (a.id_votacao > b.id_votacao)
-        return 1
-      else if (a.id_votacao < b.id_votacao)
-        return -1
-      else
-        return 0
+      if (a.id_votacao > b.id_votacao) return 1;
+      else if (a.id_votacao < b.id_votacao) return -1;
+      else return 0;
     });
   }
 
   sortProposicoesUsingArray(proposicoes, arrayTemas) {
     let proposicoesOrdered = proposicoes.sort((a, b) => {
-      let A = Number(a['tema_id']), B = Number(b['tema_id']);
+      let A = Number(a["tema_id"]),
+        B = Number(b["tema_id"]);
 
-      if (arrayTemas.indexOf(A) > arrayTemas.indexOf(B))
-        return 1;
-      else if (arrayTemas.indexOf(A) < arrayTemas.indexOf(B))
-        return -1;
-      else if (a.id_votacao > b.id_votacao)
-        return 1
-      else if (a.id_votacao < b.id_votacao)
-        return -1
-      else
-        return 0
+      if (arrayTemas.indexOf(A) > arrayTemas.indexOf(B)) return 1;
+      else if (arrayTemas.indexOf(A) < arrayTemas.indexOf(B)) return -1;
+      else if (a.id_votacao > b.id_votacao) return 1;
+      else if (a.id_votacao < b.id_votacao) return -1;
+      else return 0;
     });
 
     return proposicoesOrdered;
+  }
+
+  canVerResultados(): boolean {
+    const respostas = Object.keys(this.respostasUser.votacoes).map(
+      resposta => this.respostasUser.votacoes[resposta]
+    );
+    return (respostas.filter(r => r !== 0).length) >= this.MIN_RESPOSTAS;
   }
 
   ngOnDestroy() {
     this.unsubscribe.next();
     this.unsubscribe.complete();
   }
-
 }
