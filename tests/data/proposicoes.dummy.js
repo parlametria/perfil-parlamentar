71af4063247590ea2eb0b0aa46d484ea809fb356
gileadekelvin <gileadekelvin@gmail.com>
2019-03-21 15:31:43 -0300

    Adiciona casos para testar o cálculo do alinhamento por tema

---
+++ tests/data/proposicoes.dummy.js
@@ -0,0 +1,125 @@
+const proposicoes =
+  [
+    {
+      "id_votacao": 5632,
+      "tema_id": 1
+    },
+    {
+      "id_votacao": 6043,
+      "tema_id": 3
+    },
+    {
+      "id_votacao": 6082,
+      "tema_id": 1
+    },
+    {
+      "id_votacao": 5673,
+      "tema_id": 2
+    },
+    {
+      "id_votacao": 6074,
+      "tema_id": 2
+    },
+    {
+      "id_votacao": 16370,
+      "tema_id": 2
+    },
+    {
+      "id_votacao": 16208,
+      "tema_id": 0
+    },
+    {
+      "id_votacao": 16209,
+      "tema_id": 0
+    },
+    {
+      "id_votacao": 5513,
+      "tema_id": 0
+    },
+    {
+      "id_votacao": 16519,
+      "tema_id": 0
+    },
+    {
+      "id_votacao": 16532,
+      "tema_id": 3
+    },
+    {
+      "id_votacao": 5450,
+      "tema_id": 2
+    },
+    {
+      "id_votacao": 16155,
+      "tema_id": 5
+    },
+    {
+      "id_votacao": 5494,
+      "tema_id": 3
+    },
+    {
+      "id_votacao": 6148,
+      "tema_id": 3
+    },
+    {
+      "id_votacao": 16186,
+      "tema_id": 3
+    },
+    {
+      "id_votacao": 5520,
+      "tema_id": 3
+    },
+    {
+      "id_votacao": 55961,
+      "tema_id": 2
+    },
+    {
+      "id_votacao": 55962,
+      "tema_id": 2
+    },
+    {
+      "id_votacao": 55963,
+      "tema_id": 1
+    },
+    {
+      "id_votacao": 5624,
+      "tema_id": 1
+    }
+  ]
+
+const temasAlinhamento = [
+  {
+      "tema_id": 1,
+      "respostasIguais": 0,
+      "perguntasIguais": 0,
+      "alinhamento": 0
+  },
+  {
+      "tema_id": 3,
+      "respostasIguais": 0,
+      "perguntasIguais": 0,
+      "alinhamento": 0
+  },
+  {
+      "tema_id": 2,
+      "respostasIguais": 0,
+      "perguntasIguais": 0,
+      "alinhamento": 0
+  },
+  {
+      "tema_id": 0,
+      "respostasIguais": 0,
+      "perguntasIguais": 0,
+      "alinhamento": 0
+  },
+  {
+      "tema_id": 5,
+      "respostasIguais": 0,
+      "perguntasIguais": 0,
+      "alinhamento": 0
+  }
+]
+
+module.exports = {
+  proposicoes,
+  temasAlinhamento
+}
