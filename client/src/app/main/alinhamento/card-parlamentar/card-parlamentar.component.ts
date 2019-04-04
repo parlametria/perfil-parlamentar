95cb17737be3f71f00234de36652d7a6e7ccb5d9
Jefferson Neves <jefferson.rpn@gmail.com>
2019-04-04 15:36:44 -0300

    Anima a transição entre visualizações dos alinhamentos

--- client/src/app/main/alinhamento/card-parlamentar/card-parlamentar.component.ts
+++ client/src/app/main/alinhamento/card-parlamentar/card-parlamentar.component.ts
@@ -1,11 +1,51 @@
 import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  AnimationEvent
+} from '@angular/animations';
 
 import { Parlamentar } from 'src/app/shared/models/parlamentar.model';
 
 @Component({
   selector: 'app-card-parlamentar',
   templateUrl: './card-parlamentar.component.html',
-  styleUrls: ['./card-parlamentar.component.scss']
+  styleUrls: ['./card-parlamentar.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '*',
+        opacity: 1,
+      })),
+      state('closed', style({
+        height: '0px',
+        opacity: 0,
+      })),
+      transition('open => closed', [
+        animate('0.25s')
+      ]),
+      transition('closed => open', [
+        animate('0.25s 0.3s')
+      ]),
+    ]),
+    trigger('show', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide', style({
+        opacity: 0
+      })),
+      transition('show => hide', [
+        animate('0.25s')
+      ]),
+      transition('hide => show', [
+        animate('0.25s 0.3s')
+      ]),
+    ]),
+  ]
 })
 export class CardParlamentarComponent implements OnChanges, OnInit {
   readonly VIEW_SM = "sm";
@@ -21,11 +61,12 @@ export class CardParlamentarComponent implements OnChanges, OnInit {
   readonly FILTRO_PADRAO_TEMA = -1;
 
   alinhamento: any;
-
   following: boolean = false;
+  isCollapsed: boolean;
 
   constructor() {
     this.view = this.VIEW_MD;
+    this.isCollapsed = false;
   }
 
   ngOnInit() {
@@ -33,9 +74,9 @@ export class CardParlamentarComponent implements OnChanges, OnInit {
       this.setAlinhamento(this.FILTRO_PADRAO_TEMA);
     }
   }
-  
-  ngOnChanges(changes: SimpleChanges) {     
-    if (changes.tema !== undefined && changes.tema.currentValue !== null) {      
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.tema !== undefined && changes.tema.currentValue !== null) {
       this.setAlinhamento(changes.tema.currentValue);
     }
   }
@@ -45,11 +86,25 @@ export class CardParlamentarComponent implements OnChanges, OnInit {
     this.followChecked.emit(this.following);
   }
 
-  setAlinhamento(tema: number) {    
-    if (tema !== undefined && tema !== this.FILTRO_PADRAO_TEMA) {      
-      this.alinhamento = this.parlamentar.alinhamento.temas.filter(res => res.tema_id === tema)[0];      
+  setAlinhamento(tema: number) {
+    if (tema !== undefined && tema !== this.FILTRO_PADRAO_TEMA) {
+      this.alinhamento = this.parlamentar.alinhamento.temas.filter(res => res.tema_id === tema)[0];
     } else {
       this.alinhamento = this.parlamentar.alinhamento;
+    }
+  }
+
+  onAnimationStart(event: AnimationEvent) {
+    if (event.toState == 'closed') {
+      this.isCollapsed = true;
+    }
+  }
+
+  onAnimationEnd(event: AnimationEvent) {
+    if (event.toState == 'open') {
+      this.isCollapsed = false;
+    } else {
+      this.isCollapsed = true;
     }
   }
 
