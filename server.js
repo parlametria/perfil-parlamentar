5a3500df41a7c0fe91914d9541d159d1fc9aa431
gileadekelvin <gileadekelvin@gmail.com>
2019-04-23 09:12:42 -0300

    Adiciona permissão de cors para o ambiente de desenvolvimento do leggo

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 var db = require("./models/index");
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://front.dev.leggo.org.br', 'https://leggo.org.br'],
+  origin: ['http://localhost:3000', 'http://localhost:8080', 'https://front.dev.leggo.org.br', 'https://leggo.org.br'],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   exposedHeaders: ["authorization"]
