2600e075e073e3791be9c9a1fb2eeaea7908f271
gileadekelvin <gileadekelvin@gmail.com>
2019-04-30 15:58:03 -0300

    Altera id do app do Facebook

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: './api/',
-  facebookAppID: '2339282366084079',
+  facebookAppID: '373455540114566',
   googleAppID: '791030988243-msi1r67ltvd5v1fjtajj3un1f0c0d7ds.apps.googleusercontent.com'
 };
