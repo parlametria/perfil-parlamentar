3dea09365725712cba4d37a239d1c098080b5c63
Hadrizia Santos <hadrizia@gmail.com>
2019-05-07 15:11:16 -0300

    Update parlamentares.js

--- routes/api/parlamentares.js
+++ routes/api/parlamentares.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const Op = require("sequelize").Op;
-
 const models = require("../../models/index");
 const { formataVotacoes } = require("../../utils/functions");
 
