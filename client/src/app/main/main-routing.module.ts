af19b5340307b973488955de4a6e3657e07acbb6
gileadekelvin <gileadekelvin@gmail.com>
2019-05-07 13:14:33 -0300

    Adiciona tela de sobre e modifica navbar para incluir link para a página

--- client/src/app/main/main-routing.module.ts
+++ client/src/app/main/main-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     data: { animation: 'HomeComponent' }
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+    data: { animation: 'SobreComponent' }
   }
 ];
 
