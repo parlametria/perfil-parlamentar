9a5f9fbd0d3847f5b916ce3a7830f573784f3811
Hadrizia <hadrizia@gmail.com>
2019-07-10 11:31:23 -0300

    Move filtro de temas

--- client/src/app/filter/filter.component.ts
+++ client/src/app/filter/filter.component.ts
@@ -180,6 +180,7 @@ export class FilterComponent implements OnInit, OnDestroy {
     } else {
       this.temaSelecionado = idTema;
     }
+    this.aplicarFiltro();
   }
 
   getTemaById(id: number) {
