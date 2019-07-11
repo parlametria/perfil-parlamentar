45936c15e67c4f91bc674c911533a819ebd5371a
Jefferson Neves <jefferson.rpn@gmail.com>
2019-07-11 15:22:30 -0300

    Bugfix Exibe corretamente a barra de alinhamento quando não foi possível calcular

--- client/src/app/shared/components/progress/progress.component.ts
+++ client/src/app/shared/components/progress/progress.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.scss']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent implements OnInit, OnChanges {
   @Input() value: number;
   @Input() min: number;
   @Input() max: number;
@@ -15,6 +15,12 @@ export class ProgressComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  ngOnChanges() {
+    if (this.value < 0) {
+      this.value = 0;
+    }
+  }
 
   getClass(): string[] {
     const classes = ['progress-bar', this.class];
