8a4bd4a231bb3fbdd4a78a9305f42172eb67c8bb
Jefferson Neves <jefferson.rpn@gmail.com>
2019-11-22 11:11:07 -0300

    Mantem a casa selecionada ao navegar pelo menu principal

--- client/src/app/shared/components/navbar/navbar.component.ts
+++ client/src/app/shared/components/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { LoginService } from '../../services/login.service';
+import { CasaService } from '../../services/casa.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,23 +12,31 @@ import { LoginService } from '../../services/login.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit, OnDestroy {
-  userAuthenticated: boolean;
-  user: any;
 
   private unsubscribe = new Subject();
 
+  userAuthenticated: boolean;
+  user: any;
+  casa: string;
+
   constructor(
-    private loginService: LoginService) { }
+    private loginService: LoginService,
+    public casaService: CasaService) { }
 
   ngOnInit() {
     this.loginService.isLoggedIn().pipe(takeUntil(this.unsubscribe)).subscribe(res => {
       this.userAuthenticated = res;
       this.user = this.loginService.getCurrentUser();
     });
+    this.casaService.get().pipe(takeUntil(this.unsubscribe)).subscribe(casa => this.casa = casa);
   }
 
   logoutUser() {
     this.loginService.logoutUser();
+  }
+
+  getCasa() {
+    return this.casa;
   }
 
   ngOnDestroy() {
--- client/src/app/shared/components/sticky-footer-navbar/sticky-footer-navbar.component.ts
+++ client/src/app/shared/components/sticky-footer-navbar/sticky-footer-navbar.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { CasaService } from '../../services/casa.service';
 
 @Component({
   selector: 'app-sticky-footer-navbar',
   templateUrl: './sticky-footer-navbar.component.html',
   styleUrls: ['./sticky-footer-navbar.component.scss']
 })
-export class StickyFooterNavbarComponent {
+export class StickyFooterNavbarComponent implements OnInit {
 
-  constructor() { }
+  private unsubscribe = new Subject();
+
+  casa: string;
+
+  constructor(private casaService: CasaService) { }
+
+  ngOnInit() {
+    this.casaService.get().pipe(takeUntil(this.unsubscribe)).subscribe(casa => this.casa = casa);
+  }
+
+  getCasa() {
+    return this.casa;
+  }
 
 }
