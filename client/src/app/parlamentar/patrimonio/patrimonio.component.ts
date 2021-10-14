61447da964f333765134eb2de0bb6de7f1f30628
dehatanes <debs.outsmart@gmail.com>
2021-10-14 19:40:07 -0300

    [Feat] request para pegar patrimônio + estado de vaio e de loading

--- client/src/app/parlamentar/patrimonio/patrimonio.component.ts
+++ client/src/app/parlamentar/patrimonio/patrimonio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
+import { ParlamentarService } from '../../shared/services/parlamentar.service';
+import { PerfilPoliticoService } from '../../shared/services/perfil-politico.service';
 
 @Component({
   selector: 'app-patrimonio',
@@ -7,7 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatrimonioComponent implements OnInit {
 
-  constructor() { }
+  patrimonio: any;
+  temPatrimonio: boolean;
+  isLoading: boolean;
 
-  ngOnInit() { }
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private perfilPoliticoService: PerfilPoliticoService,
+    private parlamentarService: ParlamentarService,
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.activatedroute.parent.params.pipe(take(1)).subscribe(params => {
+      this.getPerfilPolitico(params.id);
+    });
+  }
+
+  getPerfilPolitico(id) {
+    this.parlamentarService
+      .getInfoById(id)
+      .pipe(take(1))
+      .subscribe(
+        parlamentar => {
+          if (parlamentar.idPerfilPolitico) {
+            this.perfilPoliticoService
+              .get(parlamentar.idPerfilPolitico)
+              .pipe(take(1))
+              .subscribe(
+                resp => this.handleRequestResponse(resp),
+                err => this.handleRequestError(err),
+              );
+          }
+        },
+        err => this.handleRequestError(err),
+      )
+  }
+
+  handleRequestResponse(resp) {
+    console.log(resp);
+    const patrimonio = resp.asset_history || [];
+    this.patrimonio = patrimonio;
+    this.temPatrimonio = patrimonio.length > 0;
+    this.isLoading = false;
+  }
+
+  handleRequestError(error) {
+    console.log('Erro ao buscar parlamentar: ', error);
+    this.temPatrimonio = false;
+    this.isLoading = false;
+  }
 }
